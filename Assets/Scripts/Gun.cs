7184e08c3abd0a2c9469aa3bb106c4ede4d42a62
nikitaputintsevprogramming <nikitaputintsevprogramming@gmail.com>
2022-11-27 00:38:05 +0300

    4. Создание реалистичного выстрела и урон
    Летучка ≈ 5 мин
    Настройка игрока ≈ 5 мин
    Импортирование звука для выстрела ≈ 5 мин
    Импортирование эффектов попадания ≈ 10 мин
    Скриптинг для урона ≈ 15 мин
    Скриптинг для эффектов и звука ≈  5 мин
    Добавляем сдвиг объекта ≈  10 мин
    Итоги ≈ 5 мин

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,6 +7,13 @@ public class Gun : MonoBehaviour
     public Camera cam ;
 
     public float range = 50;
+    public float MarkDamage = 10;
+
+    public ParticleSystem ShootEffect;
+    public ParticleSystem HitEffect;
+    public AudioSource ShootAudio;
+
+    public float ImpactForce = 500f;
 
     void Update()
     {
@@ -18,10 +25,27 @@ public class Gun : MonoBehaviour
 
     void Shoot()
     {
+        ShootEffect.Play();
+        ShootAudio.Play();
+
         RaycastHit hit;
         if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
         {
             Debug.Log(hit.collider.gameObject.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(MarkDamage);
+            }
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * ImpactForce);
+            }
+
+            ParticleSystem CreateHit = Instantiate(HitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(CreateHit.gameObject, 1f);
         }
     }
 }
